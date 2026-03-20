177df2f0ed40a8183dae13bcba226165b783c8b5
Pratik Chaulya <168434407+PratikChaulya97@users.noreply.github.com>
2026-03-20 21:17:36 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+       Hi recharge platform started  version 1.0
+      </h1>
+    </div>
+  );
+}
